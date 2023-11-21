152ed1f07c280410803702b1d0cdb7a8b9d590ba
jessicaravaka <jessicaravaka@gmail.com>
2023-11-21 00:41:56 -0800

    project sources

---
+++ src/main/java/com/nba/nbaMatch/NbaMatchApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nbaMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba/nbaMatch/NbaMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nbaMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
